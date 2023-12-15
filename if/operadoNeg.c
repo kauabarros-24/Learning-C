83e7427ec85f8a3ff3143f051afa5f1395e2b040
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-12-15 17:48:21 -0300

    Aprendemos a usar o operador de negação

---
+++ if/operadoNeg.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+    float media;
+    scanf("%f", &media);
+        if (media >= 7.0) {
+            printf("Aprovado\n");
+        }
+        else if (!(media >= 7.0)) {
+            printf("Reprovado");
+        }
+        else {
+            printf("Estranho...");
+        }
+        return 0;
+}
