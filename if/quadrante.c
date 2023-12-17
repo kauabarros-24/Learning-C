9ebcb65577cfe1a3d4c83bc0a54e0c51cc098a49
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-12-17 10:18:07 -0300

    Estruturas de repetição

--- if/quadrante.c
+++ if/quadrante.c
@@ -3,6 +3,21 @@
 int main () {
     const int x, y;
     scanf("%d %d", &x, &y);
+    if (x > 0 && y > 0) {
+        printf("Q1");
+    }
+    else if (x < 0 && y > 0) {
+        printf("Q2");
 
-    
+    }
+    else if (x < 0 && y < 0) {
+        printf("Q3");
+    }
+    else if (x > 0 && y < 0) {
+        printf("Q4");
+    }
+    else {
+        printf("eixos");
+    }
+    return 0;
 }
