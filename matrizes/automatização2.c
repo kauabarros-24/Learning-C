6afba19721ee6fba6c567b48fc9ac9f38eb99baf
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-12-21 14:51:54 -0300

    a

---
+++ matrizes/automatização2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int m[2][3], linha, coluna, i;
+    for (i = 0; i < 6; i++) {
+        scanf("%d", m[0]+i);
+    }
+    for (linha = 0; linha < 2; linha++) {
+        for (coluna = 0; coluna < 3; coluna++) {
+            printf("%d ", m[linha][coluna]);
+        }
+        printf("\n");
+        
+    }
+    
+
+}
